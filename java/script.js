81215be9cc8ec548fd275515a8e0226fd1c06f83
koeurkys <134634364+koeurkys@users.noreply.github.com>
2023-05-25 06:37:38 -1000

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,37 @@
+
+  
+//   contenu login
+const center = document.querySelector('.center');
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+
+btnPopup.addEventListener('click', ()=> {
+  center.classList.add('active');
+});
+
+iconClose.addEventListener('click', ()=> {
+  center.classList.remove('active');
+});
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
+});
+
+
+
