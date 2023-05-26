eaa92212a18036d8af676f9dfa95eab7dac34e20
koeurkys <134634364+koeurkys@users.noreply.github.com>
2023-05-26 00:41:19 -1000

    Update script.js

--- java/script.js
+++ java/script.js
@@ -65,3 +65,78 @@ closeMenu.addEventListener('click', ()=> {
 });
 
 
+let compteur = 0;
+let timer, elements, slides, slideWidth, speed, transition;
+
+window.onload = () => {
+    const diapo = document.querySelector('.diapo');
+
+    speed = diapo.dataset.speed;
+    transition = diapo.dataset.transition;
+
+    elements = document.querySelector('.global-reviews');
+
+    let firstImage = elements.firstElementChild.cloneNode(true);
+
+    elements.appendChild(firstImage);
+    
+    slides = Array.from(elements.children);
+
+    slideWidth = 320;
+
+    let next = document.querySelector('.skip-right');
+    let prev = document.querySelector('.skip-left');
+
+
+    next.addEventListener('click', slideNext);
+    prev.addEventListener('click', slidePrev);
+
+    timer = setInterval(slideNext, speed);
+
+    diapo.addEventListener('mouseover', stopTimer);
+    diapo.addEventListener('mouseout', startTimer);
+
+
+}
+
+function slideNext() {
+    compteur++;
+    elements.style.transition= transition+'ms linear';
+
+    let decal = -slideWidth * compteur;
+    elements.style.transform = `translateX(${decal}px)`;
+
+    setTimeout(function(){
+        if (compteur >= slides.length -1) {
+            compteur = 0;
+            elements.style.transition= 'unset';
+            elements.style.transform='translateX(0)';
+        }
+    }, transition);
+}
+
+function slidePrev() {
+    compteur--;
+    elements.style.transition = transition+'ms linear';
+
+    if (compteur < 0) {
+        compteur = slides.length -1;
+        let decal = -slideWidth * compteur
+        elements.style.transition= 'unset';
+        elements.style.transform=`translateX(${decal}px)`;
+        setTimeout(slidePrev, 1);
+    }
+
+    let decal = -slideWidth * compteur
+    elements.style.transform=`translateX(${decal}px)`;
+
+
+}
+
+function stopTimer() {
+    clearInterval(timer);
+}
+
+function startTimer() {
+    timer = setInterval(slideNext, speed);
+}
